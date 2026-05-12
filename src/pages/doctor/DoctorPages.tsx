c26542a4aaf8064a8436f869b340122e3e2afdde
KY-Hsiao <psykyh@gmail.com>
2026-05-13 01:39:27 +0800

    Use compact doctor patient page

--- src/pages/doctor/DoctorPages.tsx
+++ src/pages/doctor/DoctorPages.tsx
@@ -5,5 +5,6 @@ export {
 } from "./DoctorDashboardAndSchedulePages";
 export { DoctorRecordPage } from "./DoctorRecordPage";
 export { DoctorReturnRecordPage } from "./DoctorReturnRecordPage";
-export { DoctorLeaveRequestPage, DoctorPatientPage, DoctorRemindersPage } from "./DoctorPatientAndReminderPages";
+export { DoctorPatientPage } from "./DoctorPatientPage";
+export { DoctorLeaveRequestPage, DoctorRemindersPage } from "./DoctorPatientAndReminderPages";
 export { DoctorTeamCommunicationPage } from "./DoctorCommunicationPage";
